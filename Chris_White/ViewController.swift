2b0a4064e4bf788d68b551e8a3da0c832ae4fed9
christopher white <cmwhite1995@gmail.com>
2018-09-01 09:32:43 -0400

    bonus features

--- Chris_White/ViewController.swift
+++ Chris_White/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var backgroundcolor: UIColor!
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func DidTabButton(_ sender: Any) {
+        TextLabel.textColor = UIColor.blue
+    }
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    
+    @IBAction func DidTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func DidTapChangeText(_ sender: Any) {
+        //TextLabel.text = "Goodbye"
+        TextLabel.text = TextField.text
+        view.endEditing(true)
+        TextField.text = ""
+    }
+    @IBAction func OnReset(_ sender: Any) {
+        
+        TextLabel.text = "Hello From Chris!"
+        TextLabel.textColor = UIColor.black
+        view.backgroundColor = backgroundcolor
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backgroundcolor = view.backgroundColor
     }
 
     override func didReceiveMemoryWarning() {
